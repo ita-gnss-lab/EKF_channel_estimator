3aa56fc3cc62cd345c909da3b67208beaf6a6b70
thlobof <thiagoloboferreira650@gmail.com>
2025-09-22 14:40:22 -0300

    Used VariableFractionalDelay+circshift for refGen

--- reference_generator/reference_signal.m
+++ reference_generator/reference_signal.m
@@ -16,7 +16,22 @@ ranging_code = double(2*PRN_code - 1);
 % Samples it
 current_chip = mod(floor(time.* configuration.chippingFrequency), numel(PRN_code)) + 1;
 sampled_code = ranging_code(current_chip);  
+
+
+
+% % Computes LOS transmission phase and delay
+% doppler = configuration.dopplerProfile(2:end);
+% [LOS_phase, LOS_delay] = get_LOS_dynamics(time, [delay doppler], configuration.carrierFrequency);
 % Applies the delay
-delay_in_samples = floor(delay * configuration.samplingFrequency);
-signal = circshift(sampled_code, delay_in_samples);
+delay_in_samples = delay * configuration.samplingFrequency;
+delay_handler = dsp.VariableFractionalDelay("InterpolationMethod","Linear", 'MaximumDelay',9999);
+signal_delayed = delay_handler(sampled_code, delay_in_samples);
+signal_circ_shifted = circshift(signal_delayed, round(delay_in_samples));
+signal_delayed(1:round(delay_in_samples)) = signal_circ_shifted(1:round(delay_in_samples));
+signal = signal_delayed;
+
+
+% %Applies the delay
+% delay_in_samples = floor(delay * configuration.samplingFrequency);
+% signal = circshift(sampled_code, delay_in_samples);
 
