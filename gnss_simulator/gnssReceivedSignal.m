a09c259a225530118081e7602397c40d0ec56d15
rodrigodelimaf <rdlfresearch@gmail.com>
2025-09-25 10:17:21 -0300

    refactor(gnssReceivedSignal): Put all variables in camelCase

--- gnss_simulator/gnssReceivedSignal.m
+++ gnss_simulator/gnssReceivedSignal.m
@@ -1,4 +1,4 @@
-function [received_signal, time] = gnss_received_signal(configuration, numberOfEpochs)
+function [receivedSignal, time] = gnss_received_signal(configuration, numberOfEpochs)
 %   GNSS_RECEIVED_SIGNAL
 %   Detailed explanation goes here
 
@@ -9,24 +9,24 @@ time = (0 :  N - 1) / configuration.samplingFrequency;
 
 %% TRANSMITTED PILOT SIGNAL
 % Gets the code in bits; 0 and 1
-PRN_code = gnssCACode(configuration.satellite, "GPS");
+PRNCode = gnssCACode(configuration.satellite, "GPS");
 % Remaps to +1 and -1
-ranging_code = double(2*PRN_code - 1);
+rangingCode = double(2*PRNCode - 1);
 % Samples it
-current_chip = floor(mod((time.* configuration.chippingFrequency), numel(PRN_code)) + 1);
-sampled_code = ranging_code(current_chip);  
+currentChip = floor(mod((time * configuration.chippingFrequency), numel(PRNCode)) + 1);
+sampledCode = rangingCode(currentChip);  
 
 %% LOS PHASE AND DELAY
 % Computes LOS transmission phase and delay
-[LOS_phase, LOS_delay] = get_LOS_dynamics(time, configuration.dopplerProfile, configuration.carrierFrequency);
+[LOSPhase, LOSDelay] = get_LOS_dynamics(time, configuration.dopplerProfile, configuration.carrierFrequency);
 % Applies the delay
 % delay_in_samples = LOS_delay * configuration.samplingFrequency;
 % delay_handler = dsp.VariableFractionalDelay("InterpolationMethod","Linear", 'MaximumDelay',9999);
 % delayed_code = delay_handler(sampled_code, delay_in_samples);
-delay_in_samples = round(LOS_delay * configuration.samplingFrequency);
-delayed_code = circshift(sampled_code, delay_in_samples);
+delayInSamples = round(LOSDelay * configuration.samplingFrequency);
+delayedCode = circshift(sampledCode, delayInSamples);
 % Applies the phase
-received_signal = delayed_code .* exp(1j*LOS_phase);
+receivedSignal = delayedCode .* exp(1j*LOSPhase);
 
 %% THERMAL NOISE
 % noise = get_simple_thermal_noise(length(time), 1 / configuration.samplingFrequency, configuration.carrierToNoiseDensityRatio);
