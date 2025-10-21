ee1574c956e2f2d1a42b23f728f139906cad0081
rodrigodelimaf <rdlfresearch@gmail.com>
2025-10-21 12:30:36 -0300

    Revert "Added Doppler derivatives to the jacobian"
    This reverts commit d5b429424501ac24c9f7b2a3f3b5da55c550e4bf.

--- acquisition_tracking/auxiliary_functions/measurementFunction.m
+++ acquisition_tracking/auxiliary_functions/measurementFunction.m
@@ -1,10 +1,8 @@
-function [estimative, aux] = measurementFunction(x_k_k_1, configuration)
+function estimative = measurementFunction(x_k_k_1, configuration)
 %% Generate Autocorrelation Matrix
 delayError = x_k_k_1(1);
 channelWeights = x_k_k_1(5:end);
 channelOrder = numel(channelWeights) - 1;
-dopplerError = x_k_k_1(3);
-dopplerRateError = x_k_k_1(4);
 
 if isfield(configuration, "correlatorHalfSpan")
     correlatorHalfSpan = configuration.correlatorHalfSpan;
@@ -15,25 +13,9 @@ end
 shiftedCorrelationsMatrix = getShiftedCorrelations( ...
     delayError, correlatorHalfSpan, configuration, channelOrder);
 
-Ts = 1 / configuration.samplingFrequency;
-tapIndices = (0:channelOrder).';
-tapTimes = delayError + tapIndices * Ts;
-
-dopplerPhase = exp(1j * (dopplerError * tapTimes + ...
-    0.5 * dopplerRateError * tapTimes.^2));
-effectiveWeights = channelWeights .* dopplerPhase;
-
 %% Get Estimated Measurement
 totalPhaseError = x_k_k_1(2);
 estimative = exp(1j * totalPhaseError) * ...
-    sum(effectiveWeights.' .* shiftedCorrelationsMatrix, 2); 
-
-if nargout > 1
-    aux.shiftedCorrelations = shiftedCorrelationsMatrix;
-    aux.tapTimes = tapTimes;
-    aux.dopplerPhase = dopplerPhase;
-    aux.effectiveWeights = effectiveWeights;
-    aux.totalPhaseError = totalPhaseError;
-end
+    sum(channelWeights.' .* shiftedCorrelationsMatrix, 2); 
 
 end
--- acquisition_tracking/tracking_estimation.m
+++ acquisition_tracking/tracking_estimation.m
@@ -147,9 +147,8 @@ for k = 1 : simulationSteps
     % getting from the buildCorrelatorBank function.
     % z_hat_k = measurementFunction([zeros(4,1);1;zeros(q, 1)], ...
     %     configuration) / samplesTotal;
-    [z_hat_raw, measurementAux] = measurementFunction(x_k_k_1, ...
-        configuration);
-    z_hat_k = z_hat_raw / samplesTotal;
+    z_hat_k = measurementFunction(x_k_k_1, ...
+        configuration) / samplesTotal;
     
     if plotMeasures
         % ---- Plot routine -----
@@ -174,21 +173,10 @@ for k = 1 : simulationSteps
         sqrt(1) * exp(1j * x_k_k_1(2))  ...
     );
     phaseJacobian = 1j * z_hat_k;
-
-    shiftedCorrelations = measurementAux.shiftedCorrelations;
-    tapTimesRow = measurementAux.tapTimes.';
-    effectiveWeightsRow = measurementAux.effectiveWeights.';
-    phaseFactor = exp(1j * measurementAux.totalPhaseError);
-
-    dNuRow = 1j * tapTimesRow .* effectiveWeightsRow;
-    dNuDotRow = 1j * 0.5 * (tapTimesRow.^2) .* effectiveWeightsRow;
-
-    dopplerJacobian = phaseFactor * [ ...
-        sum(shiftedCorrelations .* dNuRow, 2), ...
-        sum(shiftedCorrelations .* dNuDotRow, 2)] / samplesTotal;
-
-    channelWeightsJacobian = phaseFactor * ...
-        (shiftedCorrelations .* measurementAux.dopplerPhase.') / samplesTotal;
+    dopplerJacobian = zeros(2*q + 1, 2);
+    channelOrder = numel(x_k_k_1(5:end)) - 1;
+    channelWeightsJacobian = exp(1j * x_k_k_1(2)) .* ...
+        getShiftedCorrelations(x_k_k_1(1), q, configuration, channelOrder) / samplesTotal;
 
     jacobian = [delayJacobian ...
         phaseJacobian ...
